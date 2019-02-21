146575184825a51f44cdeca710a726a6065431c6
IOGMID <39658222+IOGMID@users.noreply.github.com>
2019-02-21 11:58:11 +0300

    IOGMID_WORK

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -18,7 +18,7 @@ $this->registerMetaTag([
     <div class="container">
         <h2 class="main__contacts-title-text"><?= $pageParam->title ?></h2>
         <div class="row">
-            <div class="col-lg-3">
+            <div class="col-lg-4">
                 <div class="main__contacts-address-wrapper"><span class="main__contacts-address">Адрес:</span><br>
                     <p class="main__contacts-address-text"><?= $pageParam->address_1 ?></p>
                 </div>
@@ -45,7 +45,7 @@ $this->registerMetaTag([
                     <a class="main__contacts-mail" href="mailto:<?= $pageParam->email ?>"><?= $pageParam->email ?></a>
                 </div>
             </div>
-            <div class="col-lg-9">
+            <div class="col-lg-8">
                 <div class="main__contacts-map-wrapper">
                     <?= $pageParam->mapScript ?>
                 </div>
