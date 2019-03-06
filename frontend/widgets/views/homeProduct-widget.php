b9eedf869d1b9c3a177c9be7ad5037f5e26964d0
IOGMID <39658222+IOGMID@users.noreply.github.com>
2019-03-06 13:42:29 +0300

    IOGMID_WORK

--- frontend/widgets/views/homeProduct-widget.php
+++ frontend/widgets/views/homeProduct-widget.php
@@ -37,7 +37,7 @@
                     <div class="row main__quality-image-wrapper-two">
                         <div class="col-lg-12 ">
                             <div class="main__quality-product-wrapper background-grey main__quality-block-margin-bottom">
-                                <div class="main__quality-text-block-wrapper-one">
+                                <div class="main__quality-text-block-wrapper-one main__quality-text-block-wrapper-one-info">
                                 <h3><?= $firstProduct->title ?></h3>
                                 <p><?= $firstProduct->short_desc ?></p>
                                 </div>
