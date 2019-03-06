fbb455aa5454e48f72c512db99117930ca6741ad
IOGMID <39658222+IOGMID@users.noreply.github.com>
2019-03-06 13:59:30 +0300

    IOGMID_WORK

--- frontend/widgets/views/homeProduct-widget.php
+++ frontend/widgets/views/homeProduct-widget.php
@@ -37,7 +37,7 @@
                     <div class="row main__quality-image-wrapper-two">
                         <div class="col-lg-12 ">
                             <div class="main__quality-product-wrapper background-grey main__quality-block-margin-bottom">
-                                <div class="main__quality-text-block-wrapper-one main__quality-text-block-wrapper-one-info">
+                                <div class="main__quality-text-block-wrapper-one">
                                 <h3><?= $firstProduct->title ?></h3>
                                 <p><?= $firstProduct->short_desc ?></p>
                                 </div>
@@ -79,7 +79,7 @@
                     <div class="row main__quality-image-wrapper-two">
                         <div class="col-lg-12 ">
                             <div class="main__quality-product-wrapper background-white main__quality-block-margin-bottom">
-                                <div class="main__quality-text-block-wrapper-one main__quality-text-block-wrapper-one-info">
+                                <div class="main__quality-text-block-wrapper-one">
                                 <h3><?= $secondProduct->title ?></h3>
                                 <p><?= $secondProduct->short_desc ?></p>
                                 </div>
@@ -137,7 +137,7 @@
                     <div class="row main__quality-image-wrapper-two">
                         <div class="col-lg-12 ">
                             <div class="main__quality-product-wrapper background-grey main__quality-block-margin-bottom">
-                                <div class="main__quality-text-block-wrapper-one main__quality-text-block-wrapper-one-info">
+                                <div class="main__quality-text-block-wrapper-one">
                                 <h3><?= $lastProduct->title ?></h3>
                                 <p><?= $lastProduct->short_desc ?></p>
                                 </div>
